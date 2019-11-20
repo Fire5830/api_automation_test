7c877ed6c9af59d4e09f8e7e4dd4476c57a0639d
Fire5830 <18665871578@163.com>
2019-11-20 16:45:21 +0800

    修改const test 地址，改为本地 修改bug:自动化测试界面，新增、修改用例分组弹出中，接口分组改为用例分组

--- api_automation_test/settings.py
+++ api_automation_test/settings.py
@@ -165,9 +165,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'api_test',
         'USER': 'root',
-        'PASSWORD': 'lt19910301',
+        # 'PASSWORD': 'lt19910301',
+        'PASSWORD': '123456',
         # 'HOST': '120.79.232.23',
-        'HOST': '172.18.162.26',
+        # 'HOST': '172.18.162.26',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
